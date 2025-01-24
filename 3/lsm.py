1ef58797fab411afa7cd2b40b19c6cb10ab5c4f1
Nap20192 <kogay20192@gmail.com>
2025-01-24 06:04:46 +0500

    complete

--- 3/lsm.py
+++ 3/lsm.py
@@ -1,18 +1,18 @@
-import numpy as np
 from gauss import gauss
+
 def polynomial(x, coefficients, start, gap):
-    summ = 0
+    s = 0
     i = 0
     p = start
     while i < len(coefficients):
         if p not in gap:
-            summ += coefficients[i] * x**p
+            s += coefficients[i] * x**p
             i += 1
         p += 1
-    return summ
+    return s
 
 
-def least_square_method(x, y, degree, start=0, degrees = []):
+def least_square_method(x, y, degree, start=0, degrees = [],i_var = 0):
     X = []
     n = len(x)
 
@@ -20,17 +20,14 @@ def least_square_method(x, y, degree, start=0, degrees = []):
         t = []
         for i in range(start,degree+1):
             if i not in degrees:
-                print("i",i)
                 t.append(xi**i)
-        X.append(t)
-    print("X")
-    
+        X.append(t + [1] * i_var)
+    for i in X:
+        print (i)
+
     r =len(X[0])
 
-    for i in range(n):
-        print(X[i])
 
-    print(len(X),len(degrees),n)
     Xt = []
 
     for i in range(len(X[0])):
@@ -39,48 +36,49 @@ def least_square_method(x, y, degree, start=0, degrees = []):
             t.append(X[j][i])
         Xt.append(t)
             
-    print("Xt")
 
-    for i in range(len(Xt)):
-        print(Xt[i])
-    print("XtX")
+ 
 
     XtX = []
 
     for i in range(start, r+start ):
         t = []
         for j in range(start, r +start ):
-            summ = 0
+            s = 0
             for k in range(n):
-                summ += Xt[i][k] * X[k][j]
-            t.append(summ)
+                s += Xt[i][k] * X[k][j]
+            t.append(s)
         XtX.append(t)
-
-    for i in range(len(XtX)):
-        print(XtX[i])
+    
+    print()
+    for i in XtX:
+        print(i)
     XtY = []
     for i in range(start, r+start):
-        summ = 0
+        s = 0
         for j in range(n):
-            summ += Xt[i][j] * y[j]
-        XtY.append(summ)
+            s += Xt[i][j] * y[j]
+        XtY.append(s)
+    for i in XtY:
+        print(i)
 
-    for i in range(len(XtY)):
-        print(XtY[i])
-    
     coefficients = gauss(XtX, XtY)
     c = []
     for i in range(len(coefficients)):
         c.insert(0,coefficients[i])
 
-    y_pred = [polynomial(xi, coefficients , start, degrees) for xi in x]
-    residuals = [y[i] - y_pred[i] for i in range(n)]
-    ssr = sum(residuals[i]**2 for i in range(n))
+
     
     if start<0:
         print("c",c)
-        return c
+        y_pred = [polynomial(xi, c , start, degrees) for xi in x]
+        residuals = [y[i] - y_pred[i] for i in range(n)]
+        ssr = sum(residuals[i]**2 for i in range(n))
+        return c,ssr
 
     else:
         print("coefficients",coefficients)
-        return coefficients
+        y_pred = [polynomial(xi, coefficients , start, degrees) for xi in x]
+        residuals = [y[i] - y_pred[i] for i in range(n)]
+        ssr = sum(residuals[i]**2 for i in range(n))
+        return coefficients,ssr
--- 3/main.py
+++ 3/main.py
@@ -2,40 +2,47 @@ from lsm import least_square_method
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    x = [1, 2, 3, 4, 5, 6, 7, 8]
-    y = [5.4, 6.3, 8.2, 10.3, 12.6, 14.9, 17.3, 19.5]
-    gap = [0]
-    degrees = [1]
-    start = -1
-
+    x = [1, 2, 3, 4, 5]
+    y = [1.8, 5.1, 8.9, 14.1, 19.8]
+    gaps = [[0],[1]]
+    degrees = [1,2,3,4]
+    start = 0
+    r = []
+    count = 0
     plt.scatter(x, y, color='black', label='Data points')
 
     for degree in degrees:
-        coefficients = least_square_method(x, y, degree,start,gap)
-        x_fit = []
-        current = min(x)
-        step=0.3
-        while current < max(x):
-            x_fit.append(current)
-            current += step
-        y_fit = []
-        current = min(x)
-        while current < max(x):
-            current += 0.3
-            sum = 0
-            i = 0
-            p = start
-            while i <=degree-start-len(gap):
-                if p not in gap:
-                    print("p",p)
-                    print("i",i)
-                    sum += coefficients[i] * current**p
-                    i += 1
-                p += 1
-            y_fit.append(sum)
+        for gap in gaps:
+            coefficients, ssr = least_square_method(x, y, degree,start,gap)
+            print()
+            print(f'{count} Coefficients for degree {degree}: {coefficients} / SSR: {ssr} / Gap: {gap}')
+            count += 1
+            r.append(ssr)
+            x_fit = []
+            current = min(x)
+            step=0.3
+            while current < max(x):
+                x_fit.append(current)
+                current += step
+            y_fit = []
+            current = min(x)
+            while current < max(x):
+                current += 0.3
+                sum = 0
+                i = 0
+                p = start
+                while i <=degree-start-len(gap):
+                    if p not in gap:
 
-        plt.plot(x_fit, y_fit, label=f'Degree {degree}')
+                        sum += coefficients[i] * current**p
+                        i += 1
+                    p += 1
+                y_fit.append(sum)
 
+            plt.plot(x_fit, y_fit, label=f'Degree {degree} / Gap {gap}')
+    print()
+    print(f"Best fit: {r.index(min(r))}")
+    plt.grid()
     plt.xlabel('x')
     plt.ylabel('y')
     plt.legend()
